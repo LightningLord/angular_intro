e23941ed0656b181c4aa96f6a040dbcd6d662397
lightninglord <mr.lo.mhs@gmail.com>
2016-07-19 13:32:15 -0700

    cache example

--- Ajax/Exercises/using-http.js
+++ Ajax/Exercises/using-http.js
@@ -8,5 +8,25 @@
   // Create a call to $http that grabs the URL ../../data/baseball-standings.json
   // via the get method. If it succeeds, assign the data to $scope.teams.
   // If it fails, print an error to the $log
+  teamApp.controller('TeamCtrl', ['$scope', '$log', '$http', '$cacheFactory',
+    function ($scope, $log, $http, $cacheFactory){
+      var customCache = $cacheFactory.get('teamApp')
+    if (!customCache){
+      customCache = $cacheFactory('teamApp')
+    }
+    $scope.clearCache = function(){
+      customCache.removeAll()
+    }
+    $scope.getTeams = function(){
+      $http({
+        url: '../../data/baseball-standings.json',
+        method: 'get',
+        cache: customCache
+      }).then(function(response){
+        $scope.teams = response.data
+      })
 
+    }
+    }
+    ])
 })( angular );
